b21ecab1ad0ef997110adca738a15557065ba1e5
Michelle Morin and Patrick Kille <patrick@>
2020-03-10 11:58:49 -0700

    add file structure for testing

---
+++ ParcelDelivery.Tests/ModelTests/ParcelDeliveryTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Delivery.Models;
+using System;
+
+namespace Delivery.Tests
+{
+  [TestClass]
+  public class MakeFilesTests
+  {
+    [TestMethod]
+    public void MethodName_WhatItDoes_ReturnObject()
+    {
+
+    }
+
+  }
+}
